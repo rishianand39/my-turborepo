9b6cb393425fd2b311e9818fb4525ce4c3cf9eaf
Rishi Anand <rishi.rn818@gmail.com>
2025-05-16 12:50:02 +0530

    setup

---
+++ apps/multi-zone/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+      <div className={styles.left}>
+        <iframe
+        src="http://localhost:3001"
+      />
+      <iframe
+        src="http://localhost:3000"
+      />
+    </div>
+  );
+}
